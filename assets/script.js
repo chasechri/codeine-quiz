e6d657d02bb5cd7c96ded2dec6c9dee0c35afee1
chasechri <christensonchase451@gmail.com>
2022-05-24 15:46:40 -0500

    more styling

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,3 @@ const start_btn = document.querySelector(".start-btn");
 const quiz_box = document.querySelector(".quiz-box")
 
 // when start button clicked
-start_btn.onclick = ()=>{
-    quiz_box.classList.add("activeQuiz");
-}
