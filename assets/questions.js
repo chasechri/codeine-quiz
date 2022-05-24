e23565d5e4b54fc1c37e47ac44dba52a98995628
chasechri <christensonchase451@gmail.com>
2022-05-24 17:37:23 -0500

    adding questions w js

---
+++ assets/questions.js
@@ -0,0 +1,58 @@
+// creating array and passing the number, questios, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Questions 1?",
+        answers: "Answer 2",
+        options: [
+            "Answer 1",
+            "Answer 2",
+            "Answer 3",
+            "Answer 4"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Questions 2?",
+        answers: "Answer 4",
+        options: [
+            "Answer 1",
+            "Answer 2",
+            "Answer 3",
+            "Answer 4"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Questions 3?",
+        answers: "Answer 1",
+        options: [
+            "Answer 1",
+            "Answer 2",
+            "Answer 3",
+            "Answer 4"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Questions 4?",
+        answers: "Answer 4",
+        options: [
+            "Answer 1",
+            "Answer 2",
+            "Answer 3",
+            "Answer 4"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Questions 5?",
+        answers: "Answer 3",
+        options: [
+            "Answer 1",
+            "Answer 2",
+            "Answer 3",
+            "Answer 4"
+        ]
+    },
+];
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,49 @@
-const start_btn = document.querySelector(".start-btn");
-const quiz_box = document.querySelector(".quiz-box")
+const start_btn = document.querySelector(".start-btn button");
+const quiz_box = document.querySelector(".quiz-box");
 
-// when start button clicked
+// when start button clicked quit starts
+start_btn.onclick = ()=>{
+    quiz_box.classList.add("activeQuiz");
+    showQuestions(0);
+    queCounter(1);
+}
+
+let que_count = 0;
+let que_numb = 1;
+
+const next_btn = quiz_box.querySelector(".next-btn")
+
+// when next button clicked
+next_btn.onclick = ()=>{
+    if(que_count < questions.length - 1){
+        que_count++;
+        que_numb++;
+        showQuestions(que_count);
+        queCounter(que_numb);
+    }else{
+        console.log("Questions completed");
+    }
+}
+
+// getting Qs and options form array
+function showQuestions(index){
+    const que_text = document.querySelector(".que-text");
+    const option_list = document.querySelector(".option-list");
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'<span>';
+    let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
+                     + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
+                     + '<div class="option">'+ questions[index].options[2] +'<span></span></div>'
+                     + '<div class="option">'+ questions[index].options[3] +'<span></span></div>';
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+    const option = option_list.querySelectorAll(".option");
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this");
+    }
+}
+
+function queCounter(index){
+    const bottom_ques_counter = quiz_box.querySelector(".total-que");
+    let totalQuesCountTag = '<span><p>'+ index +'</p>of<p>'+ questions.length +'</p>Questions</span>';
+    bottom_ques_counter.innerHTML = totalQuesCountTag;
+}
